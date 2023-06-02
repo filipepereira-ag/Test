7895c8ec3eff426de0009e6b4058a7c82dffac3e
filipepereira-ag <filipe.pereira@agentifai.com>
2023-06-02 17:00:40 +0000

    Update

---
+++ ios/Pods/AgentifaiAssistant/AgentifaiCoreCommon.xcframework/ios-arm64/AgentifaiCoreCommon.framework/Headers/AgentifaiCoreCommon.h
@@ -0,0 +1,18 @@
+//
+//  AgentifaiCoreCommon.h
+//  AgentifaiCoreCommon
+//
+//  Created by Maurício Oliveira on 22/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AgentifaiCoreCommon.
+FOUNDATION_EXPORT double AgentifaiCoreCommonVersionNumber;
+
+//! Project version string for AgentifaiCoreCommon.
+FOUNDATION_EXPORT const unsigned char AgentifaiCoreCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AgentifaiCoreCommon/PublicHeader.h>
+
+
