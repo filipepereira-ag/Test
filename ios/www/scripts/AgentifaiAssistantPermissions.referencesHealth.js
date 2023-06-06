5238fff903b058aaa49c3d1db1f4fe0c9609ace6
Filipe Pereira <filipe.pereira@agentifai.com>
2023-06-06 14:16:26 +0100

    Removed ios

--- ios/www/scripts/AgentifaiAssistantPermissions.referencesHealth.js
+++
@@ -1,8 +0,0 @@
-﻿define("AgentifaiAssistantPermissions.referencesHealth$Sample_Customer_CS", [], function () {
-// Reference to producer 'Sample_Customer_CS' is OK.
-});
-define("AgentifaiAssistantPermissions.referencesHealth$Users", [], function () {
-// Reference to producer 'Users' is OK.
-});
-define("AgentifaiAssistantPermissions.referencesHealth", [], function () {
-});
