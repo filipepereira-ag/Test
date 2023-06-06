3c8afde114b5cd8e524da22915a6acc613101cd8
filipepereira-ag <filipe.pereira@agentifai.com>
2023-06-06 11:26:30 +0000

    Update

---
+++ ios/www/scripts/Sample_ValidationUtils_Lib.referencesHealth.js
@@ -0,0 +1,5 @@
+﻿define("Sample_ValidationUtils_Lib.referencesHealth$Users", [], function () {
+// Reference to producer 'Users' is OK.
+});
+define("Sample_ValidationUtils_Lib.referencesHealth", [], function () {
+});
